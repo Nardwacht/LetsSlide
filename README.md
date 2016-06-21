463484442cf2b39d4823f887f9a08658ce138064
ard5995 <nardbroekstra@kpnmail.nl>
2016-06-21 21:00:00 +0200

    third commit
    final changes plus complete readme

--- README.md
+++ README.md
@@ -8,8 +8,16 @@ Installation
 First of all, make sure you have jQuery included in your project.
 
 To activate the framework function, simply add a class '.left' or '.right' to your existing div that you want to animate.
-In your $(document).ready function, add a jQuery Selector with the letsSlide() function, with possible options and the direction the object has to move to as a string.
+In your $(document).ready function, add a jQuery Selector with the letsSlide() function, with the possible options and the direction the object has to move to as a string.
+
+The available options are direction, offSet and correction.
 
 This will look as follows:
 
-$('.right').letsSlide({}, 'right');
+$('.right').letsSlide({}, 'right', 1000, 50);
+
+It will animate the divs with a bouncy effect, woohoo! 
+
+If you don't want the bounciness, just leave the correction option blank.
+
+Have fun!
--- animateDiv.js
+++ animateDiv.js
@@ -1,32 +1,40 @@
+//done this to make sure the javascript is correct.
 'use strict';
 
+//added my function to jQuery functions, now only the $() selector is neccesary. 
 (function($){
 	$.fn.letsSlide = function(options, direction, offSet, correction)
 	{
+		//here we set the options of the div to a relative position, this allows us to move the divs. woohoo!
 		var settings = $.extend({
 			position : 'relative',
 		}, options);
 		
-		//var wacht = setTimeout(function(){
+		//if the user is on a tablet or an even smaller screen, it won't work. change the number 1000 to a value to your liking.
 			if(document.documentElement.clientWidth > 1000)
 			{
-					this.css(direction, '-1000px');
+					//the initial position of the div, so that it will be well out of screen bounds.
+					this.css(direction, '-' + offSet + 'px');
 
+					//here we check if the two options are valid numbers.
 					if(!isNaN(correction) || !isNaN(offSet))
 					{
+						//initialisation of the animateObject, well you guessed it, object. This allow us to easily change the animate() function.
 						var animateObject = {};
 
 						var total = correction + offSet;
-						toString(total);
 
+						//here we set the direction property of the animate() function to the distance the div has to travel and activate the fuction.
 						animateObject[direction] = '+=' + total + 'px';
 						this.animate(animateObject, 400);
 
+						//here the correction will be applied, so that your div will be at the position you want it to be.
 						animateObject[direction] = '-=' + correction + 'px';
 						this.animate(animateObject, 200);
 					}					
 			}
-		//}, 500);
+		
+		//Here we set the CSS to that of the settings variable
 		return this.css({
 			position : settings.position
 		});
